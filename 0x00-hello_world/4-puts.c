592b63fe54fecf2f122b1cd1193ba27952e75de2
Mulalo <dzedzemanem@gmail.com>
2022-06-02 14:32:43 -0700

    md

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
-/*This is to show how to use puts instead of printf*/
+/*
+*4-puts function
+ *a print substitue that does what print does
+ *this code prints the contents enclosed in th ""
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 /* this is the code that prints the string*/
