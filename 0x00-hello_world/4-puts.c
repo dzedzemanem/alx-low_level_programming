e342887e3168d371b5a7ec41277766ccb5876cdb
Mulalo <dzedzemanem@gmail.com>
2022-06-02 15:37:49 -0700

    mulalo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 /**
- *main-entry point
- *
- *return - always 0 success
+ *main-Entry point
+ *Return:Always 0 (success)
  */
 #include <stdio.h>
-int main(void)/*main entry point for the ccode*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);/*return 0 if code is successful*/
+return (0);
 }
