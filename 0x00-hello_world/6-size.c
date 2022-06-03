bfa27fdbf297f9528183cb8e2d9ec650065a654a
Mulalo <dzedzemanem@gmail.com>
2022-06-02 17:26:01 -0700

    md

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int ainteger;
 long along;
 long long alonglong;
 float afloat;
-printf("Size of char: %zu byte\n", sizeof(acharacter));
-printf("Size of int: %zu bytes\n", sizeof(ainteger));
-printf("Size of long: %zu bytes\n", sizeof(along));
-printf("Size of alonglong: %zu bytes\n", sizeof(alonglong));
-printf("Size of float: %zu bytes\n", sizeof(afloat));
+printf("Size of a char: %zu byte\n", sizeof(acharacter));
+printf("Size of an int: %zu bytes\n", sizeof(ainteger));
+printf("Size of a long int: %zu bytes\n", sizeof(along));
+printf("Size of a long long int: %zu bytes\n", sizeof(alonglong));
+printf("Size of a float: %zu bytes\n", sizeof(afloat));
 return (0);
 }
