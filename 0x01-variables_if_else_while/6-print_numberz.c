5f8109e58ad9eb2b00a68f85b575a8774718dfdd
Mulalo <dzedzemanem@gmail.com>
2022-06-06 14:29:36 -0700

    md

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <time.h>
 /**
  *main - Prints all single digit numbers
@@ -8,10 +8,10 @@
 int main(void)
 {
 int num;
-for (num = 0; num <= 9; num ++)
+for (num = 0; num <= 9; num++)
 {
 putchar((num % 10 + '0'));
 }
-putchar('\n')
+putchar('\n');
 return (0);
 }
