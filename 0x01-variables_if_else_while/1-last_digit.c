3391435cbc430afac772bc3531ee0e44bae3f9f5
Mulalo <dzedzemanem@gmail.com>
2022-06-06 12:46:47 -0700

    md

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 lst_n = n % 10;
 if (lst_n > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, lst_n);
+printf("Last digit of %d is %d and is greater than 5\n", n, lst_n);
 }
 else if (lst_n == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, lst_n);
+printf("Last digit of %d is %d and is 0\n", n, lst_n);
 }
 else if (lst_n < 6 && lst_n != 0)
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, lst_n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst_n);
 }
 return (0);
 }
