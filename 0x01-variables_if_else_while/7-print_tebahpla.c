465d3b764ee40a8f67fca9cb140169a4cad4bbae
Mulalo <dzedzemanem@gmail.com>
2022-06-06 14:38:56 -0700

    md

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Prints alphabet backwards
+ *Return:Always 0 (success)
+ */
+int main(void)
+{
+char ltr;
+for (ltr = 'z'; ltr <= 'a'; ltr--)
+{
+putchar(ltr);
+}
+putchar('\n');
+return (0);
+}
