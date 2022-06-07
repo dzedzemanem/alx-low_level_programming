d378acc100c67fc9117b3f63981bacdbe50bb1a1
Mulalo <dzedzemanem@gmail.com>
2022-06-07 10:24:28 -0700

    md

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Prints alphabet in small letters
+ *Return:Always 0 (success)
+ */
+int main(void)
+{
+putchar("_putchar");
+putchar('\n');
+}
+return (0);
+}
