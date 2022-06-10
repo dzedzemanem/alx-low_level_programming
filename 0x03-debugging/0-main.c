b78e96258cae98371078dc9fb1b1b6a613d0e501
Mulalo <dzedzemanem@gmail.com>
2022-06-10 10:57:09 -0700

    md

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,7 +7,15 @@
 int main(void)
 {
 int i;
-i = 0;
-positive_or_negative(i);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n > 0)
+{
+printf("%d is positive\n", n);
+}
 return (0);
 }
