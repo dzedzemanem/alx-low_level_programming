a269648f4c7210ee398442dc771352670cb8257b
dzedzemanem <dzedzemanem@gmail.com>
2022-06-13 07:06:39 -0700

    md

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* reset_to_98 - pointer update int to 98
+*@c : character to point to
+* Return:0 or 1
+19:35
+*/
+
+void reset_to_98(int *n);
+{
+int n=98;
+int *p=&n;
+ return (p);
+}
