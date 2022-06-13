7250b19cdab3c33eefb438ba2daffdd3ddc77a85
dzedzemanem <dzedzemanem@gmail.com>
2022-06-13 12:07:30 -0700

    md

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
 * reset_to_98 - pointer update int to 98
-*@c : character to point to
+*@n : character to point to
 * Return:0 or 1
-19:35
 */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 int n;
 int *ip;
-int *p=&n;
+int *p = &n;
+return (0);
 }
