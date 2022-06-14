630f56ef2fcefcec897e92d0bcef43effaaf8004
dzedzemanem <dzedzemanem@gmail.com>
2022-06-14 07:03:17 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - Prints every 2nd sting char
+ * @str: the string
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+int i;
+int j = 0;
+while (str[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
