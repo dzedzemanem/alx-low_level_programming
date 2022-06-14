c1839c005edcedee3b4be367bfbea778b32412ff
dzedzemanem <dzedzemanem@gmail.com>
2022-06-14 09:15:33 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print a square, followed by a new line
+ * @size: is the size of the square
+ * Return: 0
+ */
+void print_square(int size)
+{
+int a, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
