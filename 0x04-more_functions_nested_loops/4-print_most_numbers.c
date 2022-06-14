f0738674b741c893944cb0959b0fdaa4ecabf1c4
dzedzemanem <dzedzemanem@gmail.com>
2022-06-14 08:59:01 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numberes from zero to nine
+ * exlcuding two and four
+ *
+ * Return: the numbers from zero to nine only
+ */
+
+void print_most_numbers(void)
+{
+int n = 0;
+for (; n <= 9; n++)
+{
+if (n == 2 || n == 4)
+{
+continue;
+}
+else
+{
+_putchar(n + '0');
+}
+}
+_putchar('\n');
+}
