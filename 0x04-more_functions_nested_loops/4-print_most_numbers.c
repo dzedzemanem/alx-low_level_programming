64c68adc6b5e1d073e49e15713a3f513731a9658
dzedzemanem <dzedzemanem@gmail.com>
2022-06-14 10:11:40 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,14 +12,11 @@ void print_most_numbers(void)
 int n = 0;
 for (n >= 0; n <= 9; n++)
 {
-if (n == 2 || n == 4)
+if (n != 2 && n != 4)
 {
 continue;
 }
-else
-{
-_putchar(n + '0');
-}
+_putchar('0' + n);
 }
 _putchar('\n');
 }
