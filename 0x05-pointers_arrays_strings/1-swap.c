8b6c2fb9eb5ebd113510097dfa58b65760942ff8
dzedzemanem <dzedzemanem@gmail.com>
2022-06-14 01:36:13 -0700

    swapped

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * reset_to_98 - pointer update int to 98
- *@a , @b : swap values of the 2
+ *swap_int - swap to input values
+ *@a: swapped value
+ *@b: another swapped value
  * Return:0 or 1
  */
 void swap_int(int *a, int *b)
 {
-  a = b; 
+int c = *a;
+*a = *b;
+*b = c;
 }
