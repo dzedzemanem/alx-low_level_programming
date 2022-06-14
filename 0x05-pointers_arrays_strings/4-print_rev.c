d086510d63cc307c2a1c71315f342a0e6f045641
dzedzemanem <dzedzemanem@gmail.com>
2022-06-14 06:55:55 -0700

    string length

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Print in reverse
+ * @s: The string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+for (i -= 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+#include "main.h"
