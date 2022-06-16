6fef4d25fc87bd185e782e6966e848884586141f
dzedzemanem <dzedzemanem@gmail.com>
2022-06-16 11:39:36 -0700

    concetenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,8 +9,8 @@
 char *_strcat(char *dest, char *src)
 {
 int len1, len2, i;
-len1 = strlen(*dest);
-len2 = strlen(*src);
+len1 = strlen(dest);
+len2 = strlen(src);
 for (i = 0; i <= len2; i++)
 {dest[len1 + i] = src[i];
 }
