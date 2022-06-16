3fb509ebffa9801ca57f7645de02478d43d41294
dzedzemanem <dzedzemanem@gmail.com>
2022-06-16 12:56:27 -0700

    md

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,25 @@
 #include "main.h"
+#include <string.h>
+
 /**
- * _strncat - two words
- * @dest : pointer to char param
- * @src : pointer to char param
- * @n : int parameter
- * Return: *dest
+ * _strncat -  function that concatenates two strings.
+ * @src: cadena a copiar
+ * @dest: destino de la cadena.
+ * @n: number.
+ * Return: dest.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int index = 0, dest_len = 0;
+  int index = strlen(dest);
+  int a = 0;
 
-  while (dest[index++])
-    dest_len++;
-  for (index = 0; src[index] && index < n; index++)
-    dest[dest_len++] = src[index];
+  while (a < n && *src)
+    {
+      dest[index + a] = *src;
+      src++;
+      a++;
+    }
+  dest[index + a] = '\0';
   return (dest);
 }
