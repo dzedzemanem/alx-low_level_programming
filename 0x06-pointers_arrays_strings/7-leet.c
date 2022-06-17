cb8d609b4b6a08c91f940d40d066600a6fce3cea
dzedzemanem <dzedzemanem@gmail.com>
2022-06-16 20:35:30 -0700

    betty checker

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,24 +10,20 @@
 
 char *leet(char *s)
 {
-  int i;
-  char lower[] = "aeotl";
-  char upper[] = "AEOTL";
-  char num[] = "43071";
-
-  for (i = 0; s[i]; i++)
-    {
-      int h;
-
-      for (h = 0; h < 5; h++)
-	{
-	  if (s[i] != lower[h] && s[i] != upper[h])
-	    continue;
-
-	  s[i] = num[h];
-	  break;
-	}
-    }
-
-  return (s);
+int i;
+char lower[] = "aeotl";
+char upper[] = "AEOTL";
+char num[] = "43071";
+for (i = 0; s[i]; i++)
+{
+int h;
+for (h = 0; h < 5; h++)
+{
+if (s[i] != lower[h] && s[i] != upper[h])
+continue;
+s[i] = num[h];
+break;
+}
+}
+return (s);
 }
