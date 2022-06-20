6b0728c678dbe1cce83bada48637296958753be1
dzedzemanem <dzedzemanem@gmail.com>
2022-06-20 10:03:45 -0700

    mulalo

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * *_strspn - return the number of occurent of a string
+ * @s: string to check
+ * @accept: character to match
+ * Return: int
  */
 unsigned int _strspn(char *s, char *accept)
 {
-char *s = "hello, world";
-char *f = "oleh";
-unsigned int n;
-n = _strspn(s, f);
-printf("%u\n", n);
-return (0);
+int i, j, cmpt = 0;
+for (i = 0; s[i] >= '\0'; i++)
+{
+for (j = 0; accept[j] > '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+cmpt++;
+break;
+}
+}
+if (accept[j] == '\0')
+{
+break;
+}
+}
+return (cmpt);
 }
